1e043a816e55e242f2bc3450031c5fdd525f0986
BredihinLev <Cheburek_70@mail.ru>
2025-09-29 19:46:55 +0500

    Task1

---
+++ tyuiu.BredihinLI.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.BredihinLI.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Лев";
+            int age = 21;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " год! ");
+            Console.ReadKey();
+        }
+    }
+}
