ff68fa1e7a4ea8436cfc05067bccaaf3de8b6216
BredihinLev <Cheburek_70@mail.ru>
2025-09-29 23:14:52 +0500

    Task2

---
+++ tyuiu.BredihinLI.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using tyuiu.BredihinLI.Sprint0.Task2.V0.Lib;
+namespace tyuiu.BredihinLI.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Лев";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет..., Лев", res);
+        }
+    }
+}
